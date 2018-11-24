0d58e9aff9bc53c896c2e249146dbb0855a8d872
Rafal Florczak <rafal.florczak@nokia.com>
2018-11-24 14:57:04 +0100

    old style of entry point

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import HelloScreen from "./screens/HomeScreen";
+import HelloScreen from "./src/screens/HomeScreen";
 
 export default class App extends React.Component {
   render() {

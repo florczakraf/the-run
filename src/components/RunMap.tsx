f92df2966eebe96b1af7b0d36f60fbc03f50ccf1
Rafal Florczak <rafal.florczak@nokia.com>
2018-11-24 18:01:40 +0100

    render some markers on the map

--- src/components/RunMap.tsx
+++ src/components/RunMap.tsx
@@ -1,6 +1,37 @@
 import React from "react";
 import { MapView } from "expo";
 
+const someMarkers = [
+  {
+    latitude: 52.2246811,
+    longitude: 20.9439219
+  },
+  {
+    latitude: 52.2282377,
+    longitude: 21.0132706
+  },
+  {
+    latitude: 52.22707279999999,
+    longitude: 21.0015926
+  },
+  {
+    latitude: 52.1981326,
+    longitude: 20.9644923
+  },
+  {
+    latitude: 52.2306037,
+    longitude: 20.9947205
+  },
+  {
+    latitude: 52.2323258,
+    longitude: 20.9598583
+  },
+  {
+    latitude: 52.2535086,
+    longitude: 20.9913124
+  }
+];
+
 const RunMap = () => (
   <MapView
     style={{ flex: 1 }}
@@ -10,7 +41,11 @@ const RunMap = () => (
       latitudeDelta: 0.002,
       longitudeDelta: 0.232
     }}
-  />
+  >
+    {someMarkers.map((marker, i) => (
+      <MapView.Marker coordinate={marker} key={i} />
+    ))}
+  </MapView>
 );
 
 export default RunMap;

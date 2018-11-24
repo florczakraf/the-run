efba1007618b59b97b3543ae0e1e39d6e279483c
jamone- <dominik.rabij@gmail.com>
2018-11-24 15:55:23 +0100

    App pad status bar

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import HelloScreen from "./src/screens/HomeScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import { Constants } from "expo";
 
 export default class App extends React.Component {
   render() {
-    return <HelloScreen />;
+    return (
+      <View style={styles.container}>
+        <HomeScreen />
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    paddingTop: Constants.statusBarHeight
   }
 });

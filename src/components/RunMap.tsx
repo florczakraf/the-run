bd7b778d40ff9c81fc33e6e238b12be64b283c5f
Rafal Florczak <rafal.florczak@nokia.com>
2018-11-24 18:38:51 +0100

    render current location

--- src/components/RunMap.tsx
+++ src/components/RunMap.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MapView } from "expo";
+import { MapView, Permissions, Location } from "expo";
 
 const someMarkers = [
   {
@@ -32,20 +32,65 @@ const someMarkers = [
   }
 ];
 
-const RunMap = () => (
-  <MapView
-    style={{ flex: 1 }}
-    initialRegion={{
-      latitude: 52.22977,
-      longitude: 21.011788,
-      latitudeDelta: 0.002,
-      longitudeDelta: 0.232
-    }}
-  >
-    {someMarkers.map((marker, i) => (
-      <MapView.Marker coordinate={marker} key={i} />
-    ))}
-  </MapView>
-);
+class RunMap extends React.Component {
+  state = {
+    hasPermissions: false,
+    location: null
+  };
+
+  componentDidMount() {
+    this._getLocation();
+  }
+
+  _getLocation = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      this.setState({
+        location: null
+      });
+    } else {
+      this.setState({ hasPermissions: true });
+    }
+
+    let location = await Location.getCurrentPositionAsync({}); // TODO this or server? extract to method
+    setTimeout(() => {
+      this.setState({ location: location["coords"] });
+    }, 3000);
+
+    // TODO follow player on map? run on timer or socket data?
+    // this.setState({
+    //   mapRegion: {
+    //     latitude: location.coords.latitude,
+    //     longitude: location.coords.longitude,
+    //     latitudeDelta: 0.0022,
+    //     longitudeDelta: 0.0021
+    //   }
+    // });
+  };
+
+  render = () => (
+    <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: 52.22977,
+        longitude: 21.011788,
+        latitudeDelta: 0.002,
+        longitudeDelta: 0.232
+      }}
+    >
+      {someMarkers.map((marker, i) => (
+        <MapView.Marker coordinate={marker} key={i} />
+      ))}
+
+      {this.state.location ? (
+        <MapView.Marker
+          coordinate={this.state.location}
+          key="player"
+          pinColor="blue"
+        />
+      ) : null}
+    </MapView>
+  );
+}
 
 export default RunMap;

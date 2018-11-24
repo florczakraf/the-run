491e887bf305e0e71e79b4b77f4edf5d145f16be
Rafal Florczak <rafal.florczak@nokia.com>
2018-11-24 18:00:44 +0100

    normalize gmap output

--- misc/get_pois.py
+++ misc/get_pois.py
@@ -7,11 +7,19 @@ warsaw = {'lat': 52.2302300802915, 'lng': 21.0045444802915}
 five_km_radius = []
 next_page = ''
 
+
+def normalize(z):
+    geo = z['geometry']['location']
+    return {
+        'latitude': geo['lat'],
+        'longitude': geo['lng'],
+    }
+
+
 for _ in xrange(20):
     result = gmaps.places('Zabka', location=warsaw, radius=5000, page_token='')
-    five_km_radius.extend(z['geometry']['location'] for z in result['results'])
+    five_km_radius.extend(normalize(z) for z in result['results'])
     next_page = result['next_page_token']
 
 with open('zabkas.txt', 'w') as f:
     json.dump(five_km_radius, f, indent=2)
-

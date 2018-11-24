ac6c6c986e2fdbd489f6e843d1bd7baa5ca37956
Rafal Florczak <rafal.florczak@nokia.com>
2018-11-24 16:31:46 +0100

    add some pois and a script to generate them

---
+++ misc/get_pois.py
@@ -0,0 +1,17 @@
+import json
+
+import googlemaps
+
+gmaps = googlemaps.Client(key='')
+warsaw = {'lat': 52.2302300802915, 'lng': 21.0045444802915}
+five_km_radius = []
+next_page = ''
+
+for _ in xrange(20):
+    result = gmaps.places('Zabka', location=warsaw, radius=5000, page_token='')
+    five_km_radius.extend(z['geometry']['location'] for z in result['results'])
+    next_page = result['next_page_token']
+
+with open('zabkas.txt', 'w') as f:
+    json.dump(five_km_radius, f, indent=2)
+

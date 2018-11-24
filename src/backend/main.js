fee5b69f56c69b2679c405d56e92b38c2138d681
Rafal Florczak <rafal.florczak@nokia.com>
2018-11-24 22:22:20 +0100

    implement joining and leaving game, and missing response to handshake msg

--- src/backend/main.js
+++ src/backend/main.js
@@ -2,20 +2,20 @@ const server = require("http").createServer();
 const io = require("socket.io")(server);
 const fs = require("fs");
 
-const contents = fs.readFileSync("zabkas.json");
+const contents = fs.readFileSync("src/backend/zabkas.json");
 const targets = JSON.parse(contents);
 
 class Game {
   constructor(
     name, // string
     startTime, // timestamp
-    gameTime, // minutes
+    duration, // minutes
     numberOfTargets, // int
     entryFee, // decimal
     maxParticipants // int
   ) {
     this.startTime = startTime;
-    this.gameTime = gameTime;
+    this.duration = duration;
     this.name = name;
     this.entryFee = entryFee;
     this.maxParticipants = maxParticipants;
@@ -25,7 +25,7 @@ class Game {
   }
 
   hasEnded() {
-    return this.startTime + this.gameTime * 60 * 1000 <= Date.now();
+    return this.startTime + this.duration * 60 * 1000 <= Date.now();
   }
 
   isFull() {
@@ -40,9 +40,29 @@ class Game {
     if (!this.isFull()) {
       this.players.push(player);
       player.visitedTargets = this.targets.map(_, i => null);
+      player.currentGame = this.id;
     } else {
       throw "game full";
     }
+  }
+
+  drop(player) {
+    player.currentGame = null;
+    visitedTargets = {};
+    locationHistory = {};
+    this.players = this.players.filter(p => p != player);
+  }
+
+  gameInfo() {
+    return {
+      id: this.id,
+      name: this.name,
+      startTime: this.startTime,
+      duration: this.duration,
+      numberOfTargets: this.targets.length,
+      players: this.players.length,
+      entryFee: this.entryFee
+    };
   }
 }
 
@@ -55,6 +75,10 @@ const games = {
 };
 const players = {};
 
+function gamesInfo() {
+  return Object.entries(games).map(([_, game]) => game.gameInfo());
+}
+
 function handleHandshake(client, msg) {
   const id = msg.id;
   const player = {
@@ -64,13 +88,25 @@ function handleHandshake(client, msg) {
     currentGame: null, // game.id str
     visitedTargets: {} // timestamps for each target
   };
+  client.player = player;
   players = { ...players, [id]: player };
+  client.emit("games", gamesInfo());
+}
+
+function handleJoin(player, msg) {
+  const game = games[msg.gameId];
+  game.register(player);
+}
+
+function handleDrop(player, msg) {
+  const game = games[msg.gameId];
+  game.drop(player);
 }
 
 io.on("connection", function(client) {
   client.on("handshake", msg => handleHandshake(client, msg));
-  //   client.on("join", handleJoin);
-  //   client.on("drop", handleDrop);
+  client.on("join", msg => handleJoin(client.player, msg));
+  client.on("drop", msg => handleDrop(client.player, msg));
   //   client.on("locationHistory", handleLocationHistory);
 
   //   client.on("disconnect", handleDisconnect(client));

d5687b89e460b5324954c9088782840e63a325a4
jamone- <dominik.rabij@gmail.com>
2018-11-25 09:08:31 +0100

    position update delay

--- src/screens/RunScreen.tsx
+++ src/screens/RunScreen.tsx
@@ -53,7 +53,7 @@ class RunScreen extends React.Component<Props> {
     const distance = getDistance({ ...this.state.location }, { ...target });
     const step = distance / STEPS;
     const bearing = getBearing({ ...this.state.location }, { ...target });
-    const DELAY = 2000;
+    const DELAY = 1000;
 
     const points = new Array(STEPS)
       .fill(0)
